76d863f09abc86a056cd0f1baecc139d587a7ce2
salmankhattak <salman.khattak@wisetail.com>
2023-03-08 15:31:59 -0500

    fixup

--- src/web-component-a.ts
+++ src/web-component-a.ts
@@ -1,19 +1,13 @@
-const template = document.createElement('template');
-template.innerHTML = `<div class="card"><div class="card-body"></div></div>`;
 class WebComponentA extends HTMLElement {
     private _shadowRoot: ShadowRoot;
     constructor() {
         super();
         this._shadowRoot = this.attachShadow({ 'mode': 'open' });
-        this._shadowRoot.appendChild(template.content.cloneNode(true));
     }
     connectedCallback() {
-      let $townName = document.createElement('p');
-      $townName.innerHTML = `Town: My Town`;
-      this._shadowRoot.appendChild($townName);
-      let $temperature =  document.createElement('p');
-      $temperature.innerHTML = `25 &deg;C`
-      this._shadowRoot.appendChild($temperature);
+      const a = document.createElement('article');
+      a.innerHTML = `<header><h2>Header</h2></header><p>Body</p><footer><small>Footer</small></footer>`;
+      this._shadowRoot.appendChild(a);
     }
 }
 window.customElements.define('web-component-a', WebComponentA);
